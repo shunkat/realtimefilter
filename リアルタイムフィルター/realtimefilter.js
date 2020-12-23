1cbae3b18ce91c136e2d4ba6bd88a7a8ddf3489a
古濱柚実 <kohamayumi@kohamayuzujitsunoMacBook-Pro.local>
2020-12-23 21:27:58 +0900

    リアルタイムフィルター

---
+++ リアルタイムフィルター/realtimefilter.js
@@ -0,0 +1,45 @@
+$(function () {
+  searchWord = function(){
+    let searchResult,
+        searchText = $(this).val(), // 検索ボックスに入力された値をvalue値に設定
+        targetText,
+        performResult,
+        hitNum;
+
+
+    // 検索結果を格納するための配列を用意
+    searchResult = [];
+
+    // 検索結果エリアの表示を空にする
+    $('#search-result__list').empty();
+    $('.search-result__hit-num').empty();
+
+    // 検索ボックスに値が入ってる場合
+    if (searchText != '') {
+      $('.addhidden span').each(function() {
+        targetText = $(this).text();
+        performResult = $(this).parent().css("display", "none");
+
+        // 検索対象となるリストに入力された文字列が存在するかどうかを判断
+        if (targetText.indexOf(searchText) != -1) {
+          searchResult.push(targetText);
+        }else{
+          console.log("performResult");
+        }
+      });
+
+      // 検索結果をページに出力
+     //for (let i = 0; i < searchResult.length; i ++) {
+       //$('<span>').text(searchResult[i]).appendTo('#search-result__list');
+     //}
+
+      // ヒットの件数をページに出力
+      hitNum = '<span>検索結果</span>：' + searchResult.length + '件見つかりました。';
+      $('.search-result__hit-num').append(hitNum);
+    }
+    console.log("searchResult");
+  };
+
+  // searchWordの実行
+  $('#search-text').on('input',searchWord);
+});
