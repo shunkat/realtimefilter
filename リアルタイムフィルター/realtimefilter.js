339e6f22b88f70d5bc44db717646e698d73915a4
internship@niconicotravel.com <shunkat@icloud.com>
2020-12-27 19:30:26 +0900

    おかしいところを直してとりあえず動くようにしました

--- リアルタイムフィルター/realtimefilter.js
+++ リアルタイムフィルター/realtimefilter.js
@@ -18,13 +18,21 @@ $(function () {
     // 検索ボックスに値が入ってる場合
     if (searchText != '') {
       $('.addhidden span').each(function() {
+        // targetTextはaddhiden spanの各要素のそれぞれの中身の値、つまりそれぞれの検索対象
         targetText = $(this).text();
-
         // 検索対象となるリストに入力された文字列が存在するかどうかを判断
         if (targetText.indexOf(searchText) != -1) {
+          // 合った場合はsearchResultの配列の中にtarget.textを入れる
           searchResult.push(targetText);
+          // 何故か条件が合致した方にもhiddenクラスがつけられてしまっていたのでそれを打ち消す感じでshowクラスに対するcssを定義してそれを付け足すようにしました
+          $(this).parent().addClass("show");
+
         }else{
-          performResult = $(this).addClass("hidden");
+          // なかったらクラスにhiddenを付け足して隠す
+          performResult.push(targetText);
+          // ここでは各要素がなかったらその要素の親要素にhiddenクラスをつけてかくしたいのでこれでOK
+          $(this).parent().addClass("hidden");
+          // もとのやつだとhiddenクラスを付け足した要素をperformResultという配列にうわがきしてたので変になってた
         }
       });
 
@@ -37,8 +45,9 @@ $(function () {
       hitNum = '<span>検索結果</span>：' + searchResult.length + '件見つかりました。';
       $('.search-result__hit-num').append(hitNum);
     }
-    console.log("searchResult");
-    console.log("performResult");
+    // ここで表示したいのは文字列ではないのでダブルクオーテーションはいらない
+    console.log(searchResult);
+    console.log(performResult);
   };
 
   // searchWordの実行
